598f6f3c5f90d05bc8c47aabdafbd40d23c7b00e
Matt Horn <mathphreak@gmail.com>
2016-01-16 13:53:17 -0600

    Explain controls at main menu

--- js/menu.js
+++ js/menu.js
@@ -4,9 +4,11 @@ TRCGame.menuState = {
   create: function () {
     var game = TRCGame.game
     this.nameLabel = game.add.text(20, 20, 'TRC Hackathon\nGame',
-            {font: '30px League Spartan', fill: '#ffffff'})
+      {font: '30px League Spartan', fill: '#ffffff'})
+    this.controls = game.add.text(20, 150, 'Z to jump, X to shoot',
+      {font: '20px League Spartan', fill: '#ffffff'})
     this.startLabel = game.add.text(20, 200, 'Press Z to start',
-            {font: '30px League Spartan', fill: '#ffffff'})
+      {font: '30px League Spartan', fill: '#ffffff'})
 
     var zKey = game.input.keyboard.addKey(Phaser.Keyboard.Z)
 
