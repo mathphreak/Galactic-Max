306ffc59bce5ddb01798791d20c2cf6390179785
Matt Horn <mathphreak@gmail.com>
2016-01-15 21:31:32 -0600

    Make losing more emotionally painful

--- js/lose.js
+++ js/lose.js
@@ -2,8 +2,16 @@
 
 var loseState = {
   create: function () {
+    var abusiveMessages = [
+      'Get good',
+      'Just uninstall'
+    ]
+
     this.nameLabel = game.add.text(20, 20, 'You lose!',
             {font: '30px League Spartan', fill: '#ffffff'})
+    this.failLabel = game.add.text(20, 90,
+      abusiveMessages[Math.floor(Math.random() * abusiveMessages.length)],
+      {font: '30px League Spartan', fill: '#ffffff'})
     this.winLabel = game.add.text(20, 200, 'Press Z to restart',
             {font: '30px League Spartan', fill: '#ffffff'})
 
